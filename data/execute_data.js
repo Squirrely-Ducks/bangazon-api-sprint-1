501941115b1ef5a459a60b8b23d991762a749c54
Eduardo DuQuesne <eduquesne@gmail.com>
2018-03-21 15:47:25 -0500

    Changed Files

--- data/execute_data.js
+++ data/execute_data.js
@@ -1,6 +1,11 @@
 const { execute_prod_data } = require(`./product_data/build_product_data`);
 const { execute_cust_data } = require(`./customer_data/build_customer_data`);
+const { execute_employee_data } = require(`./employee_data/build_employee_data`);
+const { execute_prod_type_data } = require(`./product_type_data/build_product_type_data`);
 
 
 execute_prod_data();
 execute_cust_data();
+execute_employee_data();
+execute_employee_data();
+execute_prod_type_data();
--- db/build_db.js
+++ db/build_db.js
@@ -2,6 +2,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const { build_prod_table, insert_prod_data } = require('./build_products');
 const { build_cust_table, insert_cust_data } = require('./build_customers');
+const  { build_prod_type_table, insert_prod_type_data } = require('./build_product_type');
+const  { build_employee_table, insert_employee_data} = require('./build_employee');
 
 //instantiate database and call individual table create and populate data gsfunctions
   build_prod_table()
@@ -9,9 +11,20 @@ const { build_cust_table, insert_cust_data } = require('./build_customers');
       return insert_prod_data();
     })
     .then(()=>{
-      return
-      build_cust_table();
+      return build_cust_table();
     })
     .then(()=>{
       return insert_cust_data();
+    })
+    .then (() => {
+      return build_prod_type_table();
+    })
+    .then(() => {
+      return insert_prod_type_data();
+    })
+    .then(() => {
+      return build_employee_table();
+    })
+    .then(() => {
+      return insert_employee_data();
     });
