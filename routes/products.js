d9ca79b4a64b3d353561135b40bab58ce15750dd
kylesaldana5 <kylesaldana5@gmail.com>
2018-03-23 10:53:34 -0500

    fixed getting one specific product

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,6 @@ const productRouter = Router();
 const {get_products, get_one_product }= require('../controllers/product_ctrl');
 
 productRouter.get('/products', get_products);
-productRouter.get('/products/:id');
+productRouter.get('/products/:id', get_one_product);
 
 module.exports = productRouter;
